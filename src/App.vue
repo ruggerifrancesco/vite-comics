656da6fb85432972c3d063af7cf48a89dd45b4e8
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-15 15:40:29 +0200

    defined componenets in vue app main

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
 
+  export default {
+    components: {
+      AppHeader,
+      AppMain,
+      AppFooter
+    }
+  }
 </script>
 
 <template>
